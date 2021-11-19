a483645bb8d0a45260efb25aafacb5a3e149dc08
TheProtagonist <45369668+TheProtaganist@users.noreply.github.com>
2021-11-19 13:15:36 -0500

    Update Generate.py

--- Generate.py
+++ Generate.py
@@ -1,9 +1,9 @@
-from CodeGen import GenerationJ, Generation3
+from CodeJ import GenerationJ, Generation3
 
 lang = "Python"
-temperature = 0.9
+temperature = 0.6
 top_p = 1.0
 
 
-J = GenerationJ("Temp")
+J = GenerationJ("file_name")
 J.generate("Python", temperature, top_p, "Make a square", "Make a triangle using turtle")
