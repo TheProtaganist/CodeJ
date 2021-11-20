518d7569b3b67c73503beae9549b603a17143904
TheProtagonist <45369668+TheProtaganist@users.noreply.github.com>
2021-11-19 19:43:20 -0500

    Delete Generate.py

--- Generate.py
+++
@@ -1,9 +0,0 @@
-from CodeJ import GenerationJ, Generation3
-
-lang = "Python"
-temperature = 0.6
-top_p = 1.0
-
-
-J = GenerationJ("file_name")
-J.generate("Python", temperature, top_p, "Make a square", "Make a triangle using turtle")
